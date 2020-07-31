5b5cfc6ef905a88d867fb15a09d180713fdc148c
Santiago Gómez <santiagoskull@gmail.com>
2020-07-31 22:00:29 +0000

    Practice Develop an SAP Fiori App Using ABAP

---
+++ TRAVEL_APP_SGR/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("namespace_sgr.travel_app_sgr.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
